75780ead08bad8a4dbe8e8fed4af5f96556f8277
xiao luo <xiao.luo@thoughtworks.com>
2020-11-05 23:24:24 +0800

    [Luoxiao] fead:  add AgeSerializer

--- src/main/java/com/thoughtworks/capability/gtb/util/TimeSerializer.java
+++ src/main/java/com/thoughtworks/capability/gtb/util/TimeSerializer.java
@@ -3,6 +3,7 @@ package com.thoughtworks.capability.gtb.util;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
 import java.io.IOException;
 import java.util.Date;
 
--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.thoughtworks.capability.gtb.util.AgeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PersonVo {
 
-  private String id;
-  private Integer age;
-  private String name;
-  private String hobby;
+    private String id;
+    @JsonSerialize(nullsUsing = AgeSerializer.class)
+    private Integer age;
+    private String name;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String hobby;
 }
